6b58b1e5dee2ce0fd4e0f7ad0cf5e15a28989112
Ross <ross@rosss-mbp.lan>
2022-12-29 12:31:10 -0800

    homework2

--- Home work 2.py
+++ Home work 2.py
@@ -1,12 +1,19 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome(executable_path='./chromedriver')
-driver.get('https://www.amazon.com/')
-driver.find_element(By.XPATH, "//a[@href='/gp/css/order-history?ref_=nav_orders_first']")
 
-driver.find_element(By.ID,id="ap_email").send_keys("nasrinrafi2000@gmail.com")
-driver.find_element((By.ID,"continue"))
-driver.find_element(By.XPATH,"//span[@class='a-expander-prompt']")
+driver = webdriver.Chrome(executable_path='/Users/ross/Desktop/python-selenium-automation/chromedriver')
+driver.maximize_window()
+driver.get('https://www.amazon.com/')
+driver.find_element(By.ID,'twotabsearchtextbox').send_keys('zippo')
+driver.find_element(By.XPATH, 'span[@class="nav-cart-icon nav-sprite"]')
+driver.find_element(By.XPATH ,'span[@class="a-size-base a-text-bold"]')
+Input = driver.find_element(By.Id ,'input[@type="text]')
+Input.send_keys('nasrin')
+password = driver.find_element(By.XPATH,'lable[@for="ap_email"]')
+password.send_keys('Hello')
+checkbox =driver.find_element(By.XPATH,'i[@class="a-icon a-icon-checkbox]').click()
+driver.find_element(By.ID,'continue').click()
 
 
 
--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome(executable_path='./chromedriver')
+driver = webdriver.Chrome(executable_path='/Users/ross/Desktop/python-selenium-automation/chromedriver')
 driver.maximize_window()
 
 # open the url
