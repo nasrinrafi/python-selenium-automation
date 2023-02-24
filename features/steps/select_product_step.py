3db76d8c15dc2ad72cd07e14cddb37b081cb8e13
Ross <ross@rosss-mbp.lan>
2023-02-23 17:32:50 -0800

    select_product

---
+++ features/steps/select_product_step.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+from selenium import webdriver
+
+@given( 'open amazon product{product_id} page')
+def open_page(context,product_id):
+     context.driver.get('https://www.amazon.com/gp/product/product_id/')
+@then('able to select product through color')
+def select_product(context):
+     expected_color=['Black','Blue, Over Dye','Light Wash',
+                     'Bright White','Dark Blue Vintage',
+                     'Dark Indigo/Rinsed','Dark Khaki Brown',]
+     actual_color=[]
+     colors = context.driver.find_element(By.CSS_SELECTOR,'#variation_color_name')
+     for color in colors[:9]:
+          color.click()
+     color_name=context.driver.find_element(By.CSS_SELECTOR,'.selection').text
+     actual_color+=[color_name]
+     assert actual_color==color_name
---
+++ features/tests/select_product.feature
@@ -0,0 +1,8 @@
+# Created by ross at 2/22/23
+Feature: user able to choose product and verify with color
+  # Enter feature description here
+
+  Scenario:user able to select color
+  Given open amazon product B07BJKRR25 page
+  Then able to select product through color
+    # Enter steps here
